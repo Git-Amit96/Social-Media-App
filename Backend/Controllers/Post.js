5cfe97d4cca3e7f3956a50a247b50f9d8a59734d
Git-Amit96 <amitsaini962561@gmail.com>
2025-01-09 20:27:33 +0530

    New CommentAndLike API created

--- Backend/Controllers/Post.js
+++ Backend/Controllers/Post.js
@@ -1,7 +1,7 @@
 const cloudinary = require("../Utils/Cloudinary.config");
 const getDataUri = require("../Utils/DataURI");
 const Post = require("../Models/post.model");
-const Profile = require("../Models/user.model");
+const Comment = require("../Models/comment.model");
 
 const CreatePost = async (req, res) => {
     try {
@@ -29,7 +29,6 @@ const CreatePost = async (req, res) => {
     }
 }
 
-
 const GetUserPosts = async (req, res) => {
     try {
         const loggedUser = req.user;
@@ -39,7 +38,18 @@ const GetUserPosts = async (req, res) => {
 
         const allPosts = await Post.find({
             author: loggedUser._id
-        }).select("media capion like comments").skip(skip).limit(limit);
+        }).select("media capion like comments").populate("author", "Name").populate({
+            path: "comments", // Populate the comments array
+            select: "text user", // Include only 'text' and 'user' fields from Comment
+            populate: {
+                path: "user", // Populate the 'user' field inside each comment
+                select: "Name", // Include only 'Name' field from the Profile
+            },
+        }).populate({
+            path: "like",
+            select: "Name"
+        })
+        .skip(skip).limit(limit);
         if (!allPosts) {
             return res.status(400).json({ success: "Failed", message: "You haven't created any post. Make your post" });
         }
@@ -51,29 +61,35 @@ const GetUserPosts = async (req, res) => {
         res.status(500).json({ status: "Failed", message: "Error occured! Please try again" });
     }
 
-
 }
 
 const GetFeedPosts = async (req, res) => {
     try {
-        
+
         const loggedUser = req.user;
         const limit = req.query.limit;
         const page = req.query.page;
         const skip = (page - 1) * limit;
 
-        const feedPosts= await Post.find({
+        const feedPosts = await Post.find({
 
             $or: [
                 { author: loggedUser._id }, // Posts by logged user
-                { author: { $ne: loggedUser._id} }, 
+                { author: { $ne: loggedUser._id } },
             ],
-            
-        }).populate("author", "Name").skip(skip).limit(limit);
-        console.log(feedPosts);
+
+        }).populate("author", "Name").populate({
+            path: "comments", // Populate the comments array
+            select: "text user", // Include only 'text' and 'user' fields from Comment
+            populate: {
+                path: "user", // Populate the 'user' field inside each comment
+                select: "Name", // Include only 'Name' field from the Profile
+            },
+        }).skip(skip).limit(limit);
         const response = new Array();
-        feedPosts.forEach((entry)=>{
+        feedPosts.forEach((entry) => {
             response.push({
+                _id: entry._id,
                 author: entry.author.Name,
                 media: entry.media,
                 likes: entry.like.length,
@@ -81,7 +97,43 @@ const GetFeedPosts = async (req, res) => {
                 date: new Date(entry.createdAt.getTime()),
             })
         });
-        return res.status(200).json({ success: "Success", message: "Your Feed", data: response});
+        return res.status(200).json({ success: "Success", message: "Your Feed", data: response });
+
+    } catch (error) {
+        res.status(500).json({ status: "Failed", message: "Error occured! Please try again" });
+    }
+}
+
+const CommentAndLike = async (req, res) => {
+    try {
+        const loggedUser = req.user;
+        const { postId, like, comment } = req.body;
+
+        if (!postId || (!comment && !like)) {
+            return res.status(400).json({ status: "Failed", message: "Invalid input" });
+        }
+
+        // Fetch the post data
+        const PostData = await Post.findById(postId);
+        if (!PostData) {
+            return res.status(404).json({ status: "Failed", message: "Post not found" });
+        }
+        let commentObj;
+        if (comment) {
+            commentObj = new Comment({
+                text: comment.toString(),
+                user: loggedUser._id,
+                postId: postId,
+            })
+            let commentResp = await commentObj.save();
+            PostData.comments.push(commentResp._id);
+        }
+
+        if (like && !PostData.like.includes(loggedUser._id)) {
+            PostData.like.push(loggedUser._id);
+        }
+        await PostData.save();
+        return res.status(200).json({ success: true, message: "comment added successfully" });
 
     } catch (error) {
         res.status(500).json({ status: "Failed", message: "Error occured! Please try again" });
@@ -89,4 +141,4 @@ const GetFeedPosts = async (req, res) => {
 }
 
 
-module.exports = { CreatePost, GetUserPosts, GetFeedPosts };
+module.exports = { CreatePost, GetUserPosts, GetFeedPosts, CommentAndLike };
--- Backend/Routes/post.router.js
+++ Backend/Routes/post.router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { CreatePost, GetUserPosts, GetFeedPosts } = require("../Controllers/Post.js");
+const { CreatePost, GetUserPosts, GetFeedPosts, CommentAndLike } = require("../Controllers/Post.js");
 const isSignIn = require("../Middlewares/isSignIn.js");
 const upload = require("../Middlewares/multer.config.js");
 
@@ -8,6 +8,7 @@ const postRouter = express.Router();
 postRouter.post("/create-post", isSignIn, upload.array("uploadedPost", 5), CreatePost);
 postRouter.get("/get-user-posts", isSignIn, GetUserPosts);
 postRouter.get("/get-feed-posts", isSignIn, GetFeedPosts);
+postRouter.post("/post-comments", isSignIn, CommentAndLike);
 
 module.exports = postRouter;
 
